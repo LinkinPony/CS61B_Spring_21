7f05c8308ae4c56dc331f6c4fa0c87e0a819cbc4
LinkinPony <friendshipgame714@gmail.com>
2022-10-20 20:13:12 +0800

    proj 1

--- proj1/deque/Deque.java
+++ proj1/deque/Deque.java
@@ -1,2 +1,28 @@
-package deque;public interface Deque {
+package deque;
+
+import java.util.Iterator;
+import java.util.Objects;
+
+public interface Deque<T> {
+    public void addFirst(T item);
+
+    public void addLast(T item);
+
+    public boolean isEmpty();
+
+    public int size();
+
+    public void printDeque();
+
+    public T removeFirst();
+
+    public T removeLast();
+
+    public T get(int index);
+
+    public T getRecursive(int index);
+
+    public Iterator<T> iterator();
+
+    public boolean equals(Object o);
 }
--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,2 +1,156 @@
-package deque;public class LinkedListDeque {
+package deque;
+
+import java.util.*;
+
+public class LinkedListDeque<T> implements Deque<T>, Iterable<T> {
+    class Node {
+        T value;
+        Node prev, next;
+
+        Node(T val, Node pre, Node nxt) {
+            value = val;
+            prev = pre;
+            next = nxt;
+        }
+    }
+
+    class LLDequeIterator implements Iterator<T> {
+        Node cur;
+
+        LLDequeIterator(Node u) {
+            cur = u;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return cur != sentinel;
+        }
+
+        @Override
+        public T next() {
+            T value = cur.value;
+            cur = cur.next;
+            return value;
+        }
+    }
+
+    /**
+     * Circular Sentinel.<br>
+     * sentinel.next -> first node.<br>
+     * sentinel.prev -> last node.<br>
+     * In any time, sentinel must not be null. And if deque is empty, sentinel is both the first and the last node.
+     */
+    protected Node sentinel;
+    int size;
+
+    public LinkedListDeque() {
+        size = 0;
+        sentinel = new Node(null, null, null);
+        sentinel.next = sentinel.prev = sentinel;
+    }
+
+    public void addFirst(T item) {
+        Node newNode = new Node(item, sentinel, sentinel.next);
+        sentinel.next.prev = newNode;
+        sentinel.next = newNode;
+        size++;
+    }
+
+    public void addLast(T item) {
+        Node newNode = new Node(item, sentinel.prev, sentinel);
+        sentinel.prev.next = newNode;
+        sentinel.prev = newNode;
+        size++;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        Node cur = sentinel.next;
+        boolean space = false;
+        while (cur != sentinel) {
+            if (space) System.out.print(" ");
+            space = true;
+            System.out.print(cur.value);
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+
+    /* TODO: ensure there is no memory leak */
+    public T removeFirst() {
+        if (size > 0) size--;
+        else return null;
+        T value = sentinel.next.value;
+        Node delNode = sentinel.next;
+        sentinel.next = delNode.next;
+        delNode.next.prev = sentinel;
+        return value;
+    }
+
+    public T removeLast() {
+        if (size > 0) size--;
+        else return null;
+        T value = sentinel.prev.value;
+        Node delNode = sentinel.prev;
+        sentinel.prev = delNode.prev;
+        delNode.prev.next = sentinel;
+        return value;
+    }
+
+    public T get(int index) {
+        if (index < 0) return null;
+        int count = 0;
+        Node cur = sentinel.next;
+        while (count < index && cur != sentinel) {
+            count++;
+            cur = cur.next;
+        }
+        if (count != index || cur == sentinel) return null;
+        else return cur.value;
+    }
+
+    private T getRecursive(int index, Node cur) {
+        if (index == 0) return cur.value;
+        return getRecursive(index - 1, cur.next);
+    }
+
+    public T getRecursive(int index) {
+        if (index < 0) return null;
+        return getRecursive(index, sentinel.next);
+    }
+
+    public Iterator<T> iterator() {
+        return new LLDequeIterator(sentinel.next);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (o == this) return true;
+        else if (!(o instanceof Deque)) {
+            return false;
+        } else {
+            boolean ok = true;
+            Deque<?> tar = (Deque<?>) o;
+            if(tar.size() != this.size())ok = false;
+            else{
+                Iterator<T>itThis = this.iterator();
+                Iterator<?>itTar = tar.iterator();
+                while (itTar.hasNext() && itThis.hasNext()){
+                    if(!itTar.next().equals(itThis.next())){
+                        ok = false;
+                        break;
+                    }
+                }
+            }
+            return ok;
+        }
+    }
 }
